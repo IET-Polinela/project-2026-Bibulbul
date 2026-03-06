ef3cf129b99e4b7f4caea951b769047a7f06c244
ArkoFernanda <arkofernanda31@gmail.com>
2026-03-07 03:17:07 +0700

    Lab_Session01

---
+++ iet_24782006_2026/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+def welcome(request):
+    return HttpResponse("Selamat Datang")
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
